67042de50449eddf1d840128b97d6e3c22ec49c5
Vrockster123 <vvvrock@gmail.com>
2016-06-28 22:43:30 +0530

    Added UNDO,Alert Dialog while incrementing/Decrementing Day

--- app/src/main/java/com/example/vrock/attendancemanager2/MainActivity.java
+++ app/src/main/java/com/example/vrock/attendancemanager2/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.vrock.attendancemanager2;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -89,6 +92,19 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, (int)System.currentTimeMillis(), intent, 0);
+
+        Notification n = new Notification.Builder(this)
+                .setContentTitle("Class Tracker")
+                .setContentText("Update your Attendance")
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true).build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0,n); 
 
         dbHelper = new AttendanceDbHelper(this);
         Cursor cursor = dbHelper.getAllSubjects();
@@ -200,6 +216,9 @@ public class MainActivity extends AppCompatActivity {
                     Paper.book().write("thursday", monday);
                     friday.put(name, 0);
                     Paper.book().write("friday", monday);
+                    Stack<Integer> tempStack;
+                    tempStack = new Stack<Integer>();
+                    subjectStackArrayList.add(tempStack);
                     mAdapter.notifyDataSetChanged();
 
             }
